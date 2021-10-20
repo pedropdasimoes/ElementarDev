8b3285fcc71271b7a7aaeaae6fa82e54ab694e3e
Pedro Simões <pedropasimoes@gmail.com>
2021-10-20 19:23:59 -0300

    1.31

--- assets/js/color.js
+++ assets/js/color.js
@@ -50,6 +50,6 @@ function myFunction(x) {
     }
   }
   
-  var x = window.matchMedia("(max-width: 770px)")
+  var x = window.matchMedia("(max-width: 750px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,12 @@
-
-
-
 const elements = document.getElementsByClassName('gnTitle');
 
 for (let i = 0; i<=elements.length; i++)
 {
-    setTimeout(() => {
-        elements[i].classList.remove('hidden')
-    }, 100 *i);
-
     elements[i].addEventListener('animationend',function(e){
         elements[i].classList.remove('animated');
-        elements[i].classList.remove('animate__bounceIn');
     });
 
     elements[i].addEventListener('mouseover',function(e){
         elements[i].classList.add('animated')
     })
 }
-
-
