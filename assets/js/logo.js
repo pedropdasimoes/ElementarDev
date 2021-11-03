c7d16eba0001dcccea883ce29904307822303bf0
Pedro Simões <pedropasimoes@gmail.com>
2021-11-03 19:28:08 -0300

    1.51

--- assets/js/logo.js
+++
@@ -1,9 +0,0 @@
-
-var image = document.getElementById('imageLogo');
-
-image.addEventListener('mouseover', function() {
-    image.src = 'assets/image/logoNeon.png';
-})
-image.addEventListener('mouseout', function() {
-    image.src = 'assets/image/logo.png';
-})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-const elements = document.getElementsByClassName('gnTitle');
 
-for (let i = 0; i<=elements.length; i++)
-{
-    elements[i].addEventListener('animationend',function(e){
-        elements[i].classList.remove('animated');
-    });
+    let elements = document.getElementsByClassName('gnTitle');
 
-    elements[i].addEventListener('mouseover',function(e){
-        elements[i].classList.add('animated')
-    })
-}
+    for (let i = 0; i<=elements.length; i++)
+    {
+        if(elements){
+            elements[i].addEventListener('animationend',function(e){
+                elements[i].classList.remove('animated');
+            });
+
+            elements[i].addEventListener('mouseover',function(e){
+                elements[i].classList.add('animated')
+            });
+        }
+    }
